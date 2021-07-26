1b0dace5b018f34d1628bd5854d0d98676d7470c
ChrisKp1710 <christian.laura1710@gmail.com>
2021-07-27 01:30:37 +0200

    Spotify-app first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import './App.scss';
+
+function App() {
+  return (
+    <div className="oterWrap">
+      <div className="App">
+        <div className="navBar">
+          <div className="logo">
+            logo
+          </div>
+          <ul>
+            <li>Home</li>
+            <li>Search</li>
+            <li>Your Library</li>
+          </ul>
+        </div>
+      </div>
+      <div className="musicControles"> music controls</div>
+    </div>
+  );
+}
+
+export default App;
